9803773433c2f0079660159610e48fad625dce90
Dan Goldin <dangoldin@gmail.com>
2013-02-02 14:27:30 -0500

    Added better comparison approach

--- compare_bills.py
+++ compare_bills.py
@@ -3,22 +3,62 @@ from itertools import combinations
 
 import difflib
 
+from n_gram_splitter import lang_model
+
 data_dir = 'bills'
 
 MIN_LINES = 200
+MIN_TRIGRAMS = 3
+MIN_BIGRAMS = 3
+
+def memoize(f):
+    """ Memoization decorator for functions taking one or more arguments. """
+    class memodict(dict):
+        def __init__(self, f):
+            self.f = f
+        def __call__(self, *args):
+            return self[args]
+        def __missing__(self, key):
+            ret = self[key] = self.f(*key)
+            return ret
+    return memodict(f)
+
+@memoize
+def get_comparison_set(fi):
+    f = open(fi,'r')
+    t = f.read()
+    l = lang_model(t)
+    p = set()
+    for phrase, cnt in l.bi_fd.iteritems():
+        if cnt >= MIN_BIGRAMS:
+            p.add(phrase)
+        else:
+            break
+    return p
+
+def print_set_difference(s1, s2):
+    phrases = sorted(list(s1.union(s2)))
+    for phrase in phrases:
+        in_1 = 'X' if phrase in s1 else ' '
+        in_2 = 'X' if phrase in s2 else ' '
+        print '%s %s : %s' % (in_1, in_2, phrase)
 
 def compare(file1, file2):
-    f1 = open( file1, 'r')
-    f2 = open( file2, 'r')
+    #s = difflib.SequenceMatcher(None, t1, t2)
+    #score = s.ratio()
 
-    t1 = f1.read()
-    t2 = f2.read()
+    p1 = get_comparison_set(file1)
+    p2 = get_comparison_set(file2)
 
-    s = difflib.SequenceMatcher(None, t1, t2)
+    score = float(len(p1.intersection(p2)))/len(p1.union(p2))
 
-    print '%s,%s: %f' % (file1,file2,round(s.ratio(), 3))
+    if score > 0:
+        print '%s vs %s' % (file1, file2)
+        print_set_difference(p1,p2)
 
-    return (file1, file2, s.ratio())
+    print '%s,%s: %f' % (file1,file2,round(score, 3))
+
+    return (file1, file2, score)
 
 def keep_file(fi):
     f = open(fi, 'r')
@@ -36,6 +76,15 @@ def keep_pair(f1, f2):
     return True
 
 if __name__ == '__main__':
+    # compare('bills/ARB00002682.txt','bills/ARB00002682.txt')
+    # exit()
+
+    # compare('test/test-bill1.txt','test/test-bill2.txt')
+    # exit()
+
+    compare('bills/ARB00002682.txt', 'bills/FLB00002163.txt')
+    exit()
+
     files = [os.path.join(data_dir,f) for f in os.listdir('bills')]
 
     keep_files = [f for f in files if keep_file(f)]
---
+++ n_gram_splitter.py
@@ -0,0 +1,69 @@
+# n-gram-splitter
+
+""" Has a class called langmodel that takes a text and creates an NLTK FreqDist object
+    that has counts of unigrams, bigrams, and trigrams.
+
+    Example Usage:
+
+    s = "This is a test string. This is a test string. This is a test string. This is a test string. This is a test string. This is a test string. This is a test string. This is a test string."
+    test_lang_model = lang_model(s)
+
+    To access word counts of a specific n-gram:
+    - e.g. for unigrams
+    test_lang_model.uni_fd.count(<<WORD>>)
+
+    List of Strings
+    test_lang_model.uni_fd.keys()
+
+    List of Tuples with (STRING, COUNT)
+    test_lang_model.uni_fd.items()
+"""
+
+from nltk.corpus import stopwords
+from nltk import FreqDist, wordpunct_tokenize, cluster
+
+
+def _sliding_window(l, n):
+    return [tuple(l[i:i + n]) for i in range(len(l) - n + 1)]
+
+
+def _remove_stopwords_lowercase(words):
+    """
+    Remove stopwords, convert all words to lowercase, exclude numbers
+    """
+    return [w.lower() for w in words if not w.lower()
+        in stopwords.words('english') and w.isalpha()]
+
+
+def make_ngram_tuples(l, n):
+    l = _remove_stopwords_lowercase(l)
+    t = _sliding_window(l, n)
+    phrase_list = []
+    if n == 1:
+        phrase_list = [s for (s,) in t]
+    else:
+        list_of_tuples = [(tuple(s[:-1]), s[-1]) for s in t]
+        for phrase in list_of_tuples:
+            #print phrase
+            if n == 2:
+                phrase_list.append("%s %s" % (phrase[0][0], phrase[1]))
+            if n == 3:
+                phrase_list.append("%s %s %s" % (phrase[0][0], phrase[0][1], phrase[1]))
+    return phrase_list
+
+
+class lang_model():
+    """Creates a language model of given text"""
+    def __init__(self, text):
+        tokens = wordpunct_tokenize(text)
+        self.unigram = make_ngram_tuples(tokens, 1)
+        self.bigram = make_ngram_tuples(tokens, 2)
+        self.trigram = make_ngram_tuples(tokens, 3)
+        self.uni_fd = FreqDist(self.unigram)
+        self.bi_fd = FreqDist(self.bigram)
+        self.tri_fd = FreqDist(self.trigram)
+
+if __name__ == '__main__':
+    s = "This is a test string. This is a test string. This is a test string. This is a test string. This is a test string. This is a test string. This is a test string. This is a test string."
+    test_lang_model = lang_model(s)
+    print test_lang_model.uni_fd.items()
